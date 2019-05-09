a4901a4ad2c126120f2718a16272e7b1abf8c720
Vladimir Kuznetsov <mistakster@gmail.com>
2019-05-09 21:38:54 +0500

    implemented the connector

---
+++ test/index.js
@@ -0,0 +1,61 @@
+const { merge } = require('rxjs');
+const { map, share, flatMap } = require('rxjs/operators');
+const { create } = require('rxjs-spy');
+const { tag } = require('rxjs-spy/operators');
+const connectMongoDb = require('../lib/connect');
+
+const {
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+  MONGODB_URL,
+  MONGODB_DATABASE
+} = process.env;
+
+const MONGODB_OPTS = {
+  auth: {
+    user: MONGODB_USER,
+    password: MONGODB_PASSWORD
+  },
+  ssl: true,
+  connectWithNoPrimary: true,
+  useNewUrlParser: true,
+  bufferMaxEntries: 0,
+  connectTimeoutMS: 5000
+};
+
+const spy = create();
+
+spy.log(/^test:/);
+
+const client$ = connectMongoDb(MONGODB_URL, MONGODB_OPTS)
+  .pipe(
+    tag('test:client'),
+    share()
+  );
+
+const db$ = client$
+  .pipe(
+    map(client => client.db(MONGODB_DATABASE)),
+    tag('test:database')
+  );
+
+const logic$ = client$
+  .pipe(
+    flatMap(client => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(client.close());
+        }, 5000);
+      });
+    }),
+    tag('test:logic')
+  );
+
+merge(db$, logic$)
+  .pipe(
+    tag('test:merged')
+  )
+  .subscribe({
+    error: spy.teardown.bind(spy),
+    complete: spy.teardown.bind(spy)
+  });
